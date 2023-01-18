5a6703e3d2a72e18ded1b4ffa26545b9c5f2af11
ram098 <42148293+ram098@users.noreply.github.com>
2023-01-19 00:10:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var express = require('express');
 var app = express();
 
 app.get('/',(req,res) =>{
+   console.log("hello");
    res.send("Hello World");
 })
+
+console.log("hello kumar");
 
 http.createServer(app).listen(3000,() => {
     console.log(`server started running at port 3000`);
